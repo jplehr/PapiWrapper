8f7e5de2d6b2e66d9a9f9f9f4338c9b930297642
Mittelstädt, Janis <janis.mittelstaedt@stud.tu-darmstadt.de>
2020-08-26 17:21:46 +0200

    add additional events

--- PapiInstance.cpp
+++ PapiInstance.cpp
@@ -19,20 +19,44 @@ int getEnvEventCode(char *event_code) {
 		{ "BR_INS",  PAPI_BR_INS },
 		{ "BR_MSP",  PAPI_BR_MSP },
 		{ "BR_NTK",  PAPI_BR_NTK },
+		{ "BR_PRC",  PAPI_BR_PRC },
 		{ "FP_INS",  PAPI_FP_INS },
 		{ "FP_OPS",  PAPI_FP_OPS },
+		{ "L1_DCA",  PAPI_L1_DCA },
 		{ "L1_DCM",  PAPI_L1_DCM },
+		{ "L1_ICA",  PAPI_L1_ICA },
 		{ "L1_ICM",  PAPI_L1_ICM },
+		{ "L1_TCA",  PAPI_L1_TCA },
+		{ "L1_TCM",  PAPI_L1_TCM },
 		{ "L2_DCA",  PAPI_L2_DCA },
 		{ "L2_DCM",  PAPI_L2_DCM },
 		{ "L2_DCR",  PAPI_L2_DCR },
 		{ "L2_DCW",  PAPI_L2_DCW },
 		{ "L2_ICH",  PAPI_L2_ICH },
+		{ "L2_ICR",  PAPI_L2_ICR },
+		{ "L2_ICA",  PAPI_L2_ICA },
 		{ "L2_ICM",  PAPI_L3_ICM },
+		{ "L2_TCA",  PAPI_L2_TCA },
 		{ "L2_TCM",  PAPI_L2_TCM },
+		{ "L2_TCR",  PAPI_L2_TCR },
+		{ "L2_TCW",  PAPI_L2_TCW },
+		{ "L2_LDM",  PAPI_L2_LDM },
+		{ "L2_STM",  PAPI_L2_STM },
+		{ "L3_DCA",  PAPI_L3_DCA },
+		{ "L3_DCM",  PAPI_L3_DCM },
+		{ "L3_DCR",  PAPI_L3_DCR },
+		{ "L3_DCW",  PAPI_L3_DCW },
+		{ "L3_ICA",  PAPI_L3_ICA },
+		{ "L3_ICR",  PAPI_L3_ICR },
+		{ "L3_ICM",  PAPI_L3_ICM },
+		{ "L3_TCA",  PAPI_L3_TCA },
+		{ "L3_TCR",  PAPI_L3_TCR },
+		{ "L3_TCW",  PAPI_L3_TCW },
 		{ "L3_TCM",  PAPI_L3_TCM },
+		{ "L3_LDM",  PAPI_L3_LDM },
 		{ "LD_INS",  PAPI_LD_INS },
 		{ "SR_INS",  PAPI_SR_INS },
+		{ "LST_INS", PAPI_LST_INS },
 		{ "STL_ICY", PAPI_STL_ICY },
 		{ "TLB_IM",  PAPI_TLB_IM },
 		{ "TOT_INS", PAPI_TOT_INS },
@@ -111,7 +135,7 @@ void PapiW_stopAndPrint() {
 			std::string s(token);
 			events.push_back(s);
 		}
-		token = strtok(NULL, delim);		
+		token = strtok(NULL, delim);
 	}
 
 	delete[] codes_buffer;
